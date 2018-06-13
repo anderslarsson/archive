dc15350de3ab7aee9739fba4621055bd2435e2c6
Dennis Bücker <dennis.buecker@opuscapita.com>
2018-06-13 09:43:10 +0200

    + Changed webpack production config + Added webpack-cli dependency

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,56 +2,57 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-//        main: './src/client/components/RedisCommander/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, './src/server/static'),
-        publicPath: '/static',
-        filename: 'components/archive-[name].js',
-        library: 'archive-[name]',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
+  entry: [
+    './src/client/index.js'
+  ],
 
-    bail: true,
+  output: {
+    path: path.resolve(__dirname, './src/server/static'),
+    publicPath: '/static',
+    filename: 'bundle.js'
+    // filename: 'components/[name]-bundle.js',
+    // library: 'archive-[name]',
+    // libraryTarget: 'umd',
+    // umdNamedDefine: true
+  },
 
-    plugins: [
-        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en|de/)
-    ],
+  bail: true,
 
-    resolve: {
-        modules: ['node_modules'],
-        extensions: ['.js']
-    },
+  plugins: [
+  ],
 
-    resolveLoader: {
-        modules: ['node_modules'],
-        extensions: ['.js']
-    },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js']
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            },
-            {
-                test: /.jsx?$/,
-                loader: 'babel-loader',
-                include: [
-                    path.join(__dirname, 'src')
-                ],
-                options: {
-                    compact: true,
-                    presets: [
-                        ['env', { 'targets': { 'node': 8, 'uglify': true }, 'modules': false }],
-                        'stage-0',
-                        'react'
-                    ],
-                    plugins: ['transform-decorators-legacy']
-                }
-            }
-        ]
-    }
+  resolveLoader: {
+    modules: ['node_modules'],
+    extensions: ['.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        include: [
+          path.join(__dirname, 'src')
+        ],
+        options: {
+          compact: true,
+          presets: [
+            ['env', { 'targets': { 'node': 8, 'uglify': true }, 'modules': false }],
+            'stage-0',
+            'react'
+          ],
+          plugins: ['transform-decorators-legacy']
+        }
+      }
+    ]
+  }
 };
