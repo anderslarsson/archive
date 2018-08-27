9740c8036f3347091f3fdfc4c1c82a27ffe42175
thebilge <ibrahimbilge7@gmail.com>
2018-08-27 11:03:26 +0200

    added a sample query screen

---
+++ src/client/api/Elastic.js
@@ -0,0 +1,21 @@
+import ApiBase from './ApiBase';
+
+class Elastic extends ApiBase {
+
+    getTenantOptions() {
+        // TODO fetch tenant IDs
+        return this.ajax.get('/dummy-elastic/tenants').then(res => res.body).catch(this.getErrorFromResponse);
+    }
+
+    getYearOptions(tenantId) {
+        // TODO fetch archive years of tenant
+        return this.ajax.get(`/dummy-elastic/${tenantId}/years`).then(res => res.body).catch(this.getErrorFromResponse);
+    }
+
+    queryInvoiceArchive(queryParams) {
+        // TODO query table data with selectedValues...
+        return this.ajax.post('/dummy-elastic/query').send(queryParams).then(res => res.body).catch(this.getErrorFromResponse);
+    }
+}
+
+export default Elastic;
