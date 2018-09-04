da36da68ffe33af0b193967a6abac49a6b063deb
Dennis Bücker <dennis.buecker@opuscapita.com>
2018-09-04 15:07:55 +0000

    webpack dev config adjustment

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,46 +1,49 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
-  entry: [
-    './src/client/index.js'
-  ],
+    entry: [
+        './src/client/index.js'
+    ],
 
-  output: {
-    path: path.resolve(__dirname, './src/server/static'),
-    filename: 'bundle.js',
-    publicPath: '/static'
-  },
+    output: {
+        path: path.resolve(__dirname, './src/server/static'),
+        filename: 'bundle.js',
+        publicPath: '/static'
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      },
-      {
-        test: /.jsx?$/,
-        include: [
-          path.join(__dirname, 'local'),
-          path.join(__dirname, 'src')
-        ],
-        loader: 'babel-loader',
-        options: {
-          compact: false,
-          presets: [
-            [
-              'env', {
-                'targets': { 'node': 8, 'uglify': true }, 'modules': false 
-              }
-            ],
-            'stage-0',
-            'react'
-          ],
-          plugins: ['transform-decorators-legacy']
-        }
-      }
-    ]
-  }
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /.jsx?$/,
+                include: [
+                    path.join(__dirname, 'local'),
+                    path.join(__dirname, 'src')
+                ],
+                loader: 'babel-loader',
+                options: {
+                    compact: false,
+                    presets: [
+                        [
+                            'env', {
+                                'targets': {
+                                    'node': 8,
+                                    'uglify': false
+                                },
+                                'modules': false
+                            }
+                        ],
+                        'stage-0',
+                        'react'
+                    ],
+                    plugins: ['transform-decorators-legacy']
+                }
+            }
+        ]
+    }
 
 };
 
