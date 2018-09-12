0f223f52467e071a98e37884e2f0e640c1798fb4
Dennis Bücker <dennis.buecker@opuscapita.com>
2018-09-12 17:18:20 +0200

    Added importer for m-files customers.

---
+++ src/importers/mfiles/customers/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const IkeaCustomerBuilder = require('./lib/IkeaCustomerBuilder');
+const SiemensCustomerBuilder = require('./lib/SiemensCustomerBuilder');
+const ProcountorCustomerBuilder = require('./lib/ProcountorCustomerBuilder');
+
+async function main() {
+    try {
+        let ikea = new IkeaCustomerBuilder();
+        let siemens = new SiemensCustomerBuilder();
+        let pc = new ProcountorCustomerBuilder();
+
+        await ikea.init();
+        await siemens.init();
+        await pc.init();
+
+        /* Create mapping for customers */
+        let ikeaResult       = await ikea.run();
+        let siemensResult   = await siemens.run();
+        let procountorResult = await pc.run();
+
+        await ikea.createArchiveConfig(ikeaResult);
+        await siemens.createArchiveConfig(siemensResult);
+        await pc.createArchiveConfig(procountorResult);
+
+        debugger;
+    } catch (e) {
+        /* handle error */
+        debugger;
+    }
+}
+
+main();
