ee2ed6c7e1f7eaf78e188ea8904f2f0a5e84324a
Dennis Bücker <dennis.buecker@opuscapita.com>
2018-09-25 13:44:21 +0000

    Adds target env selection for mfiles file importer

--- src/importers/mfiles/files/Api.js
+++ src/importers/mfiles/files/Api.js
@@ -10,6 +10,8 @@ class Api {
         this.accessToken = null;
         this.initialized = false;
 
+        this.targetEnv = 'devbox';
+
         let env = dotenv.config({path: path.resolve(process.cwd(), '.env.local')});
         if (env.error) {
             const msg = 'Failed to load secrects from ENV. Can not login.';
@@ -18,7 +20,38 @@ class Api {
         }
     }
 
-    async init() {
+    get hostsConfig() {
+        return {
+            devbox: {
+                host: 'localhost',
+                port: '8080',
+                scheme: 'http',
+                username: process.env.TOKEN_AUTH_USERNAME,
+                password: process.env.TOKEN_AUTH_PASSWORD,
+                clientSecret: process.env.TOKEN_AUTH_CLIENT_SECRET_DEV
+            },
+            dev: {
+                host: 'develop.businessnetwork.opuscapita.com',
+                port: '443',
+                scheme: 'https',
+                username: process.env.TOKEN_AUTH_USERNAME,
+                password: process.env.TOKEN_AUTH_PASSWORD,
+                clientSecret: process.env.TOKEN_AUTH_CLIENT_SECRET_DEV
+            },
+            stage: {
+                host: 'stage.businessnetwork.opuscapita.com',
+                port: '443',
+                scheme: 'https',
+                username: process.env.TOKEN_AUTH_USERNAME,
+                password: process.env.TOKEN_AUTH_PASSWORD_STAGE,
+                clientSecret: process.env.TOKEN_AUTH_CLIENT_SECRET_STAGE
+            }
+        };
+    }
+
+    async init(targetEnv = 'devbox') {
+        this.targetEnv = targetEnv;
+
         if (this.initialized !== false) {
             return true;
         }
@@ -38,13 +71,23 @@ class Api {
         return this.initialized = true;
     }
 
+    applyBaseUrl(pathOrUrl) {
+        if (pathOrUrl.startsWith('http')) {
+            return pathOrUrl;
+        } else {
+            let config = this.hostsConfig[this.targetEnv];
+
+            return `${config.scheme}://${config.host}:${config.port}${pathOrUrl}`;
+        }
+    }
+
     async postJson(url, json) {
         if (!this.initialized) {
             await this.init();
         }
 
         let options = {
-            url,
+            url: this.applyBaseUrl(url),
             method: 'POST',
             headers: {
                 'Authorization': 'Bearer ' + this.accessToken
@@ -80,7 +123,7 @@ class Api {
 
         return new Promise((resolve, reject) => {
             let options = {
-                url: url,
+                url: this.applyBaseUrl(url),
                 method: 'PUT',
                 headers: {
                     'Authorization': 'Bearer ' + this.accessToken,
@@ -113,7 +156,7 @@ class Api {
 
     async fetchApiAccessToken() {
         let options = {
-            url: 'http://localhost:8080/auth/token',
+            url: this.applyBaseUrl('/auth/token'),
             method: 'POST',
             headers: {
                 'Authorization': `Basic ${process.env.TOKEN_AUTH_BEARER}`
--- src/importers/mfiles/files/FileProcessor.js
+++ src/importers/mfiles/files/FileProcessor.js
@@ -250,7 +250,7 @@ class FileProcessor {
      */
     uploadFile(data, transactionId, tenantId, filename) {
         filename = encodeURI(filename);
-        return api.putChunked(`http://localhost:8080/blob/api/${tenantId}/data/private/archive/${transactionId}/${filename}?createMissing=true`, data);
+        return api.putChunked(`/blob/api/${tenantId}/data/private/archive/${transactionId}/${filename}?createMissing=true`, data);
     }
 
 }
