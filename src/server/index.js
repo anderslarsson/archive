7be744c4f8222ed32a6b1b34c6208e56fe762350
Dennis Bücker <dennis.buecker@opuscapita.com>
2019-02-14 14:29:52 +0000

    Debugging

--- src/server/index.js
+++ src/server/index.js
@@ -31,6 +31,8 @@ if (isProduction) {
 async function init() {
     const db = await dbInit.init();
 
+    logger.log('XXX--', db.config);
+
     await server.init({
         server: {
             port: process.env.port || 3031,
